c22cc27b364cd00e5166d67ddfbfa5160f9ede1d
unknown <prathamkakkad18@gmail.com>
2023-07-23 01:48:16 +0530

    features added

--- index.js
+++ index.js
@@ -1,60 +1,60 @@
-// var operations = ["add", "sub", "mul", "div"];
+var num1, num2;
 
-// var num1 = 5;
-// var num2 = 10;
+document.querySelector(".number-entry").addEventListener("click", function () {
+  num1 = parseInt(prompt("Enter Number 1 : "));
+  num2 = parseInt(prompt("Enter Number 2 : "));
 
-// for (var i = 0; i < document.querySelectorAll(".add-img").length; i++) {
-//   document
-//     .querySelectorAll(".add-img")
-//     [i].addEventListener("click", function (num1, num2, operator) {
-//       var operator1 = operations[i];
-//       return operator(num1, num2);
-//     });
-// }
-
-// function add(num1, num2) {
-//   alert(num1 + num2);
-// }
-
-// function sub(num1, num2) {
-//   alert(num1 - num2);
-// }
-
-// function mul(num1, num2) {
-//   alert(num1 * num2);
-// }
-// function div(num1, num2) {
-//   alert(num1 / num2);
-// }
-
-var operations = ["add", "sub", "mul", "div"];
-
-var num1 = 5;
-var num2 = 10;
+  document.querySelector(".numbers-selected").innerHTML =
+    "Current Numbers: <br /> number 1 =" + num1 + "<br /> number 2 = " + num2;
+});
 
 for (var i = 0; i < document.querySelectorAll(".add-img").length; i++) {
-  (function (index) {
-    document
-      .querySelectorAll(".add-img")
-      [index].addEventListener("click", function () {
-        var operator = operations[index];
-        return window[operator](num1, num2);
-      });
-  })(i);
+  document
+    .querySelectorAll(".add-img")
+    [i].addEventListener("click", function () {
+      var buttonInfo = this.innerHTML;
+
+      var result = resultCalculation(buttonInfo);
+
+      document.querySelector(".result").innerHTML =
+        "Result after " + buttonInfo + " = " + result;
+    });
+}
+
+function resultCalculation(buttonInfo) {
+  switch (buttonInfo) {
+    case "add":
+      var result = add(num1, num2);
+      break;
+    case "sub":
+      var result = sub(num1, num2);
+      break;
+    case "mul":
+      var result = mul(num1, num2);
+      break;
+    case "div":
+      var result = div(num1, num2);
+      break;
+
+    default:
+      alert("something went wrong");
+      break;
+  }
+  return result;
 }
 
 function add(num1, num2) {
-  alert(num1 + num2);
+  return num1 + num2;
 }
 
 function sub(num1, num2) {
-  alert(num1 - num2);
+  return num1 - num2;
 }
 
 function mul(num1, num2) {
-  alert(num1 * num2);
+  return num1 * num2;
 }
 
 function div(num1, num2) {
-  alert(num1 / num2);
+  return num1 / num2;
 }
